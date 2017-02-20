4730f366bbcd146043f1c452663d6f0254a49286
jj <you@example.com>
2017-02-20 08:03:17 +0100

    V 1.2 pondeli zkopirovano

--- dohled.sh
+++ dohled.sh
@@ -12,7 +12,7 @@ echo "Přijatá data: "$rx
 tx=`ifconfig eth0 | grep Přijato | awk -F "(" '{print $3}' | awk -F ")" '{print $1}'`
 echo "Odeslaná data: "$tx
 
-echo "<html>"> godovsky.html
+echo "<html>" > godovsky.html
 echo "<body>">> godovsky.html
 echo $cas>> godovsky.html
 echo "<br>">> godovsky.html
@@ -20,6 +20,13 @@ echo $ip>> godovsky.html
 echo "<br>">> godovsky.html
 echo $rx>> godovsky.html
 echo "<br>">> godovsky.html
-echo $xr>> godovsky.html
-echo "</body>">> godovsky.html
-echo "</html>">> godovsky.html
+echo $tx>> godovsky.html
+echo "<br>">> godovsky.html
+echo "</body>">>godovsky.html
+echo "</html>">>godovsky.html
+
+
+
+
+
+
--- ip.sh
+++ ip.sh
@@ -1,7 +1,13 @@
-ip=`ifconfig | grep Všesměr | awk -F ":" '{print $2}'| awk -F " " '{print $1}'`
-echo "Moje IP: " $ip
+#!/bin/bash
 
-rx=`ifconfig eth0 | grep Přijato | awk -F  "(" '{print $2}' | awk -F ")" '{print $1}'`
+cas=`date +%T`
+echo "Aktuální čas: "$cas
+
+ip=`ifconfig | grep Všesměr | awk -F ":" '{print $2}' | awk -F " " '{print $1}'`
+echo "Moje IP: "$ip
+
+rx=`ifconfig eth0 | grep Přijato | awk -F "(" '{print $2}' | awk -F ")" '{print $1}'`
 echo "Přijatá data: "$rx
 
-tx=`ifconfig eth0 | grep Přijato | awk -F "(" '{print $3}'
+tx=`ifconfig eth0 | grep Přijato | awk -F "(" '{print $3}' | awk -F ")" '{print $1}'`
+echo "Odeslaná data: "$tx
